65b9842d59bda24364a5c046d727599e60696fd9
Reem Abutalib <reem.abutalib@gmail.com>
2024-10-20 12:05:05 +0100

    Add files via upload

---
+++ eldenring.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "You Died"
+
+#First beast battle
+
+beast=$(( $RANDOM % 2 ))
+
+echo "Your first beast approaches. Prepare to battle. Pick a number between 0-1. (0/1)"
+
+read tarnished
+
+if [[ $beast == $tarnished ]]; then
+	echo "Beast VANQUISHED! Congrats fellow tarnished"
+else
+	echo "You died"
+fi
